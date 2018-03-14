9ba1c1b06e9e3599b05108ec2065304f5d2021d9
zhoumingzhu <zg-zmz@zg18.com>
2018-03-14 17:16:58 +0800

    zhou

---
+++ src/components/user.vue
@@ -0,0 +1,18 @@
+<template>
+	<section class="userbody">
+		<img src="../assets/images/userpic.png">
+	</section>
+
+</template>
+<script type="text/javascript">
+	
+</script>
+<style scoped lang='scss'>
+	$unit:37.5px;
+	.userbody{
+		width:100%;
+		height:100%;
+		background:pink;
+	}
+	
+</style>
