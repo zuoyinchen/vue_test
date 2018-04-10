21fe588192ec03ae9fae57616e8bff4162eaa5d2
zhoumingzhu <zg-zmz@zg18.com>
2018-04-10 18:55:23 +0800

    分享判断

--- src/main.js
+++ src/main.js
@@ -93,29 +93,7 @@ new Vue({
   },
   beforeCreate:function(){
       console.log("nihao");
-      function getQueryStringArgs(){
-        var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
-            args = {},
-            items = qs.length ? qs.split("&") : [],
-            item = null,
-            name = null,
-            value = null,
-            i = 0,
-            len = items.length;
-        for (i = 0; i < len; i++) {
-            item = items[i].split("=");
-            
-            // decodeURIComponent解码
-            name = decodeURIComponent(item[0]);
-            value = decodeURIComponent(item[1]);
-            
-            if (name.length) {
-                args[name] = value;
-            }
-        }
-
-        return args;
-    }
+      
      let userobj = getQueryStringArgs();
     //  userobj.id = '5acaf0554b11af29530253da';
     //  userobj.jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6W10sInd4dXNlcmluZm9zIjpbXSwic3RhclRvcGljIjpbXSwidG9BbnN3ZXIiOltdLCJzdGFyQW5zd2VyIjpbXSwiZnJpZW5kcyI6W10sImJlRnJpZW5kcyI6W10sImJldG9waWNzIjpbXSwidXNlcm5hbWUiOiI1NjJAZWd1ZXNzLmNvbSIsIm5pY2tOYW1lIjoiZm9yZ2V0IHlvdSAscmVtZW1iZXIgbWUiLCJhdmF0YXJVcmwiOiJodHRwOi8vdGhpcmR3eC5xbG9nby5jbi9tbW9wZW4vdmlfMzIvY3cxejd5U0ZiV0RpYVBlWnZpYnBSSUhac0hVVkZzNThsR0hTbmpYZlV5WWRnSlJKUmlhWWtDdzVDNVlFTXhBOEZnVTdXWUNNZER2c290NlNtQkhTMUJYVncvMTMyIiwiZW1haWwiOiI5MzZAZWd1ZXNzLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJENaYml1RnUxV1JBMTlHdGhYUG1hU081eE1QenZUc0pnejlLdEVTQlFnYU5rQzcwejRNOGtHIiwidGVtcGxhdGUiOiJkZWZhdWx0IiwibGFuZyI6ImVuX1VTIiwiY3JlYXRlZEF0IjoiMjAxOC0wNC0wNFQxMDozMzoxNi4yODZaIiwidXBkYXRlZEF0IjoiMjAxOC0wNC0wNFQxMDozMzoxNi4zMjJaIiwid3hVc2VySW5mbyI6IjVhYzRhOWVjM2ViZDg3ODg3N2UzZWI5MyIsImlkIjoiNWFjNGE5ZWMzZWJkODc4ODc3ZTNlYjkxIiwiaWF0IjoxNTIzMjM2NjQ0fQ.I7rzLRP1NFL49VzfJ27ujIazqr-bJhA_lIvtF20JYGM";
@@ -125,7 +103,7 @@ new Vue({
        localStorage.setItem('headimg',userobj.avatarUrl);//缓存用户头像
        localStorage.setItem('nickname',userobj.nickName);//缓存用户头像
        localStorage.setItem('jwt',userobj.jwt);//缓存用户头像
-       
+       localStorage.setItem('',userobj.jwt);//缓存是否是分享的
      }
     document.addEventListener('touchmove',function (ev) {  
         event.preventDefault();  
@@ -134,3 +112,28 @@ new Vue({
   }
   
 })
+
+function getQueryStringArgs(){
+    var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
+        args = {},
+        items = qs.length ? qs.split("&") : [],
+        item = null,
+        name = null,
+        value = null,
+        i = 0,
+        len = items.length;
+    for (i = 0; i < len; i++) {
+        item = items[i].split("=");
+        
+        // decodeURIComponent解码
+        name = decodeURIComponent(item[0]);
+        value = decodeURIComponent(item[1]);
+        
+        if (name.length) {
+            args[name] = value;
+        }
+    }
+
+    return args;
+}
+Vue.prototype.getQueryStringArgs = getQueryStringArgs;
