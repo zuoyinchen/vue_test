dd43307306be28071be8450fed04c5b329be0ebf
zhoumingzhu <zg-zmz@zg18.com>
2018-04-11 15:16:30 +0800

    用户信息储存之后重定向

--- src/main.js
+++ src/main.js
@@ -31,19 +31,26 @@ router.beforeEach((to, from, next) => {
     console.log("href",router);
     console.log("to",to);
     console.log("from",from);
-    const { shareUrl } = to.query;
+    const { shareUrl,id, avatarUrl,nickName,jwt} = to.query;
+    const nextpath = to.path;
+    axios.defaults.headers.common['Authorization'] = 'Bearer '+jwt ;
     console.log("query",to.query);
     console.log("params",to.params);
     console.log(shareUrl);
     if (shareUrl) {
-    const { shareUrl } = to.query;
+        const { shareUrl } = to.query;
         window.location.href="https://www.13cai.com.cn/api/v1/get_wxlogin?shareUrl="+shareUrl;
+    } else if (id) {
+        localStorage.setItem("userid",id);//缓存用户id
+        localStorage.setItem('headimg',avatarUrl);//缓存用户头像
+        localStorage.setItem('nickname',nickName);//缓存用户头像
+        localStorage.setItem('jwt',jwt);//缓存用户头像
+        next(to.path);
     } else {
-        next();
+        next()
     }
 });
 Vue.prototype.$axios = axios;
-
 Vue.component("tabnav",tabnav);//全局注册tabnav组件；
 /* eslint-disable no-new */
 Vue.config.productionTip = false
@@ -58,20 +65,18 @@ new Vue({
   mounted:function(){
     console.log("全局mounted");
     axios.defaults.headers.common['Authorization'] = 'Bearer '+localStorage.getItem('jwt');
-
-
   },
   beforeCreate:function(){
-     let userobj = getQueryStringArgs();
-     userobj.id = '5acaf0554b11af29530253da';
-     userobj.jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6W10sInd4dXNlcmluZm9zIjpbXSwic3RhclRvcGljIjpbXSwidG9BbnN3ZXIiOltdLCJzdGFyQW5zd2VyIjpbXSwiZnJpZW5kcyI6W10sImJlRnJpZW5kcyI6W10sImJldG9waWNzIjpbXSwidXNlcm5hbWUiOiI1NjJAZWd1ZXNzLmNvbSIsIm5pY2tOYW1lIjoiZm9yZ2V0IHlvdSAscmVtZW1iZXIgbWUiLCJhdmF0YXJVcmwiOiJodHRwOi8vdGhpcmR3eC5xbG9nby5jbi9tbW9wZW4vdmlfMzIvY3cxejd5U0ZiV0RpYVBlWnZpYnBSSUhac0hVVkZzNThsR0hTbmpYZlV5WWRnSlJKUmlhWWtDdzVDNVlFTXhBOEZnVTdXWUNNZER2c290NlNtQkhTMUJYVncvMTMyIiwiZW1haWwiOiI5MzZAZWd1ZXNzLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJENaYml1RnUxV1JBMTlHdGhYUG1hU081eE1QenZUc0pnejlLdEVTQlFnYU5rQzcwejRNOGtHIiwidGVtcGxhdGUiOiJkZWZhdWx0IiwibGFuZyI6ImVuX1VTIiwiY3JlYXRlZEF0IjoiMjAxOC0wNC0wNFQxMDozMzoxNi4yODZaIiwidXBkYXRlZEF0IjoiMjAxOC0wNC0wNFQxMDozMzoxNi4zMjJaIiwid3hVc2VySW5mbyI6IjVhYzRhOWVjM2ViZDg3ODg3N2UzZWI5MyIsImlkIjoiNWFjNGE5ZWMzZWJkODc4ODc3ZTNlYjkxIiwiaWF0IjoxNTIzMjM2NjQ0fQ.I7rzLRP1NFL49VzfJ27ujIazqr-bJhA_lIvtF20JYGM";
-     axios.defaults.headers.common['Authorization'] = 'Bearer '+userobj.jwt ;
-     if(userobj.id){
-       localStorage.setItem("userid",userobj.id);//缓存用户id
-       localStorage.setItem('headimg',userobj.avatarUrl);//缓存用户头像
-       localStorage.setItem('nickname',userobj.nickName);//缓存用户头像
-       localStorage.setItem('jwt',userobj.jwt);//缓存用户头像
-     }
+    //  let userobj = getQueryStringArgs();
+    //  userobj.id = '5acaf0554b11af29530253da';
+    //  userobj.jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6W10sInd4dXNlcmluZm9zIjpbXSwic3RhclRvcGljIjpbXSwidG9BbnN3ZXIiOltdLCJzdGFyQW5zd2VyIjpbXSwiZnJpZW5kcyI6W10sImJlRnJpZW5kcyI6W10sImJldG9waWNzIjpbXSwidXNlcm5hbWUiOiI1NjJAZWd1ZXNzLmNvbSIsIm5pY2tOYW1lIjoiZm9yZ2V0IHlvdSAscmVtZW1iZXIgbWUiLCJhdmF0YXJVcmwiOiJodHRwOi8vdGhpcmR3eC5xbG9nby5jbi9tbW9wZW4vdmlfMzIvY3cxejd5U0ZiV0RpYVBlWnZpYnBSSUhac0hVVkZzNThsR0hTbmpYZlV5WWRnSlJKUmlhWWtDdzVDNVlFTXhBOEZnVTdXWUNNZER2c290NlNtQkhTMUJYVncvMTMyIiwiZW1haWwiOiI5MzZAZWd1ZXNzLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJENaYml1RnUxV1JBMTlHdGhYUG1hU081eE1QenZUc0pnejlLdEVTQlFnYU5rQzcwejRNOGtHIiwidGVtcGxhdGUiOiJkZWZhdWx0IiwibGFuZyI6ImVuX1VTIiwiY3JlYXRlZEF0IjoiMjAxOC0wNC0wNFQxMDozMzoxNi4yODZaIiwidXBkYXRlZEF0IjoiMjAxOC0wNC0wNFQxMDozMzoxNi4zMjJaIiwid3hVc2VySW5mbyI6IjVhYzRhOWVjM2ViZDg3ODg3N2UzZWI5MyIsImlkIjoiNWFjNGE5ZWMzZWJkODc4ODc3ZTNlYjkxIiwiaWF0IjoxNTIzMjM2NjQ0fQ.I7rzLRP1NFL49VzfJ27ujIazqr-bJhA_lIvtF20JYGM";
+    
+    //  if(userobj.id){
+    //    localStorage.setItem("userid",userobj.id);//缓存用户id
+    //    localStorage.setItem('headimg',userobj.avatarUrl);//缓存用户头像
+    //    localStorage.setItem('nickname',userobj.nickName);//缓存用户头像
+    //    localStorage.setItem('jwt',userobj.jwt);//缓存用户头像
+    //  }
     //  router.push('/index');
     document.addEventListener('touchmove',function (ev) {  
         event.preventDefault();  
