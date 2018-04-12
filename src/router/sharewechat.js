29b10bc5afa738499c8577c859b2edb8ac9074dd
toringo <zg-gwt@zg18.com>
2018-04-12 17:36:01 +0800

    fix: remove shareUrl

--- src/router/sharewechat.js
+++ src/router/sharewechat.js
@@ -35,7 +35,7 @@ const sharewechat = {
             wx.onMenuShareAppMessage({
                 title: '筋灵十三猜', // 分享标题
                 desc: sharedesc, // 分享描述
-                link: `${url}?shareUrl=`+url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                link: `${url}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                 imgUrl: 'http://cdn.zg18.com/13cai_logo.png', // 分享图标
                 type: '', // 分享类型,music、video或link，不填默认为link
                 dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
